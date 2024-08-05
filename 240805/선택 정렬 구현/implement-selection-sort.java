4e4d33904a8e5aa47862939c36434a3c9b66f27f
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-08-05 11:02:40 +0900

    Title: 선택 정렬 구현, Time: 111ms, Memory: 8MB, Status: Passed - CodeTree

--- 240805/선택 정렬 구현/implement-selection-sort.java
+++ 240805/선택 정렬 구현/implement-selection-sort.java
@@ -1,23 +1,38 @@
 import java.io.*;
 import java.util.*;
+
+//Code Tree
 public class Main {
-    public static void main(String[] args) throws Exception{
+    static int N;
+    static int[] arr;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        N = Integer.parseInt(br.readLine());
+
         st = new StringTokenizer(br.readLine());
+        arr = new int[N];
         for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        for(int i=0; i<N-1; i++) {
+            int min = i;
+            for(int j=i+1; j<N; j++) {
+                if(arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
 
-        StringBuilder sb = new StringBuilder();
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
         for(int idx : arr) sb.append(idx).append(" ");
 
         System.out.println(sb);
-
     }
 }
