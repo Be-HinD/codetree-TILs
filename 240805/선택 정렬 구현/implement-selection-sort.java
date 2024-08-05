64fe6cc258ea0cf00e9f451cf9296b187c3d524e
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-08-05 10:59:01 +0900

    Title: 선택 정렬 구현, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240805/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for(int idx : arr) sb.append(idx).append(" ");
+
+        System.out.println(sb);
+
+    }
+}
