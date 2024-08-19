23ec9a107b878ef6302fa9ea3623c3b96c0b35a8
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-08-20 08:03:15 +0900

    Title: 앞에서부터 삭제하기 2, Time: 326ms, Memory: 21MB, Status: Passed - CodeTree

--- 240820/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 240820/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -12,22 +12,32 @@ public class Main {
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         st = new StringTokenizer(br.readLine());
+
+        arr = new int[N];
         for(int i=0; i<N; i++) {
-            pq.offer(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        while(pq.size() != 2) {
-            pq.poll();
+        double sum = arr[N-1] + arr[N-2];
+
+        pq.offer(arr[N-1]);
+        pq.offer(arr[N-2]);
+
+        //현재 평균값
+        double res = (sum - pq.peek()) / (pq.size()-1);
+
+        for(int i=N-3; i>0; i--) {
+            pq.offer(arr[i]);
+            sum += arr[i];
+
+            res = Math.max(res, (sum - pq.peek()) / (pq.size()-1));
         }
 
-        int sum = 0;
-        while(!pq.isEmpty()) {
-            sum += pq.poll();
-        }
+        String ans = String.format("%.2f", res);
 
-        double res = (double) sum / 2;
-        String formattedNumber = String.format("%.2f", res);
+        System.out.println(ans);
 
-        System.out.println(formattedNumber);
+
+
     }
 }
