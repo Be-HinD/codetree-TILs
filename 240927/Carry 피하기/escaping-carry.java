17c1552410d12ca00e6dc34d1c4993a851362abc
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-09-27 04:43:16 +0900

    Title: Carry 피하기, Time: 851ms, Memory: 150MB, Status: Passed - CodeTree

--- 240927/Carry 피하기/escaping-carry.java
+++ 240927/Carry 피하기/escaping-carry.java
@@ -23,31 +23,37 @@ public class Main {
     }
 
     private static void DFS(int p, Map<Integer,Integer> list, int cnt) {
-        //현재 선택된 원소에서 p원소를 추가 가능한지
-        res = Math.max(res, cnt);
-        if(p >= N) return;
+        // 현재 인덱스가 범위를 벗어나면 종료
+        if (p >= N) {
+            res = Math.max(res, cnt);  // 선택한 숫자의 최대 개수를 갱신
+            return;
+        }
+
+        // 현재 숫자를 선택하지 않는 경우
+        DFS(p + 1, list, cnt);
+
+        // 현재 숫자를 선택할 수 있는지 체크
         String idx = String.valueOf(arr[p]);
         boolean flag = true;
+        Map<Integer, Integer> newMap = new HashMap<>(list);  // 새로운 Map 복사
         int num = 1;
-        for(int i=idx.length()-1; i>=0; i--) {
-            int temp = list.getOrDefault(num++, 0) + (idx.charAt(i) - '0');
-            if(temp >= 10) {
+
+        // 자릿수별로 carry가 발생하는지 확인
+        for (int i = idx.length() - 1; i >= 0; i--) {
+            int temp = newMap.getOrDefault(num, 0) + (idx.charAt(i) - '0');
+            if (temp >= 10) {  // carry가 발생하면 flag를 false로 설정
                 flag = false;
                 break;
             }
+            newMap.put(num, temp);  // 자릿수별로 업데이트
+            num++;
         }
 
-        //p원소 선택한 Case 및 선택하지 않은 Case 모두 재귀호출
-        if(flag) {
-            Map<Integer, Integer> chMap = new HashMap<>();
-            num = 1;
-            for(int i=idx.length()-1; i>=0; i--) {
-                chMap.put(num, list.getOrDefault(num++, 0) + (idx.charAt(i) - '0'));
-            }
-            DFS(p+1, chMap, cnt+1);
+        // carry가 발생하지 않으면 해당 숫자를 선택한 경우로 DFS 호출
+        if (flag) {
+            DFS(p + 1, newMap, cnt + 1);
         }
 
-        DFS(p+1, list, cnt);
 
     }
 }
