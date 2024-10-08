eb6ba5b21aaa1f99ee09d4c933017faafb88acb4
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-08 15:41:40 +0900

    Title: 가능한 최댓값, Time: 266ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241008/가능한 최댓값/maximum-possible.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {return o2[0] - o1[0];});
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            pq.offer(new int[]{x,i});
+            pq.offer(new int[]{y,i});
+        }
+
+        Set<Integer> set = new HashSet<>();
+        int res = 0;
+        int cnt = 0;
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if(set.contains(cur[1])) continue;
+            res += cur[0];
+            set.add(cur[1]);
+            cnt++;
+            if(cnt == 30) break;
+        }
+
+        System.out.println(res);
+    }
+}
